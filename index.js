d0bb86d6cb783572db879b6a4ff5ac20d31a0bbf
kbgjtn <Kambingjan31@gmail.com>
2022-08-07 19:36:57 +0100

    docker-compose for production && development

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-	res.status(200).send("<h2>Hello, World!!</h2>");
+	res.status(200).send("<h2>Hello, World!!!!</h2>");
 })
 
 app.listen(port, () => console.log("Server running on port ", port));
